ddf0d91a6b2efae8fc75995e323ba4258af374eb
bface007 <bface007@gmail.com>
2016-07-01 16:07:58 +0100

    first commit

---
+++ includes/class-wp-blu-cpt-restifier-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       facebook.com/Bface.Style
+ * @since      1.0.0
+ *
+ * @package    Wp_Blu_Cpt_Restifier
+ * @subpackage Wp_Blu_Cpt_Restifier/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Blu_Cpt_Restifier
+ * @subpackage Wp_Blu_Cpt_Restifier/includes
+ * @author     Dan T. Ngossinga <bface007@gmail.com>
+ */
+class Wp_Blu_Cpt_Restifier_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
